333e49f863e46f575120312f3bbc52d31af40539
summydev <adegbitesumayah@gmail.com>
2026-04-12 16:00:51 +0100

    Fix: Limit password length to prevent bcrypt crash

--- schemas.py
+++ schemas.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, EmailStr, Field
 class UserRegister(BaseModel):
     name: str = Field(..., min_length=2, max_length=60)
     email: EmailStr
-    password: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=8, max_length=72)
 
 class UserLogin(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., max_length=72)
 
 class TokenResponse(BaseModel):
     access_token: str
