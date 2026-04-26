b88eff8eb5b0e65ea1c8cf381f0194652dbe8528
summydev <adegbitesumayah@gmail.com>
2026-04-26 14:01:07 +0100

    moved to postgres

--- database.py
+++ database.py
@@ -1,16 +1,13 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
 
-# This creates a local file named 'mylb.db' in your project folder.
-# When you go to production, replace this URL with your Neon/Supabase PostgreSQL URL!
-sqlite_file_name = "mylb.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# This tells FastAPI: "Use the Render secret if it exists. If not, use local SQLite."
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///mylb.db")
 
-# The engine is the core connection to the database
-# echo=True prints the SQL queries to your terminal, which is great for debugging
-engine = create_engine(sqlite_url, echo=True) 
+# Create the engine
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
-    # This automatically scans your models.py and creates the tables if they don't exist
     SQLModel.metadata.create_all(engine)
 
 def get_session():
